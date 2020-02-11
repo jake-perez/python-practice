aeab4d1b83bbc17d23aeaa7eec0c0f286989d4f3
Jake Perez <501068@containerstore.com>
2020-02-10 20:22:39 -0600

    Optimized fibonacci algorithm

--- src/fibonacci/fib.py
+++ src/fibonacci/fib.py
@@ -5,3 +5,16 @@ def fib_slow(n):
     if n == 1 or n == 0:
         return 1
     return fib_slow(n-1) + fib_slow(n-2)
+
+
+def fib_fast(n):
+    cache = {0: 1, 1: 1, 2: 2}
+
+    def inner_fib(m):
+        if (m in cache):
+            return cache[m]
+        value = inner_fib(m-1) + inner_fib(m-2)
+        cache[m] = value
+        return value
+
+    return inner_fib(n)
--- src/fibonacci/fib_test.py
+++ src/fibonacci/fib_test.py
@@ -1,13 +1,16 @@
 import unittest
-from fib import fib_slow
+from fib import fib_slow, fib_fast
 
 
 class FibonacciTests(unittest.TestCase):
     def test_fib_slow(self):
-        self.assertEqual(fib_slow(4), 5, '4th fibonaccis number is 5')
+        self.assertEqual(fib_slow(25), 121393, '4th fibonaccis number is 5')
 
     def test_fib_slow_value_error(self):
         self.assertRaises(ValueError, fib_slow, -2)
+
+    def test_fib_fast(self):
+        self.assertEqual(fib_fast(25), 121393)
 
 
 if __name__ == '__main__':
